06796de7e147c4700cc194daf81abf6f53d4fb6f
Austin Sims <adsims2001@gmail.com>
2016-01-25 21:59:56 -0700

    Yay server.

---
+++ RemoteVolumeControl/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RemoteVolumeControl
+{
+    class Program
+    {
+        static VolumeControl volumeControl_ = new VolumeControl();
+
+        static void Up()
+        {
+            volumeControl_.VolUp();
+            Console.WriteLine("Increased volume");
+        }
+
+        static void Down()
+        {
+            volumeControl_.VolDown();
+            Console.WriteLine("Decreased volume");
+        }
+
+        static void Mute()
+        {
+            volumeControl_.Mute();
+            Console.WriteLine("Muted");
+        }
+
+        static void Do(char ch)
+        {
+            if (ch == 'u')
+            {
+                Up();
+            } else if (ch == 'd')
+            {
+                Down();
+            } else if (ch == 'm')
+            {
+                Mute();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(8001);
+            int requestCount = 0;
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+            Console.WriteLine(" >> Server Started");
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine(" >> Accept connection from client");
+            requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    byte[] bytesFrom = new byte[65536];
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    char[] commands = Encoding.UTF8.GetString(bytesFrom).ToCharArray();
+                    foreach (char c in commands)
+                    {
+                        Do(c);
+                    }
+                    /*
+                    string serverResponse = "Last Message from client" + dataFromClient;
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                    */
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> exit");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ RemoteVolumeControl/VolumeControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RemoteVolumeControl
+{
+    public class VolumeControl
+    {
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        public void Mute()
+        {
+            keybd_event(173, 0, 0, 0);
+        }
+
+        public void VolDown()
+        {
+            keybd_event(174, 0, 0, 0);
+        }
+
+        public void VolUp()
+        {
+            keybd_event(175, 0, 0, 0);
+        }
+    }
+}
