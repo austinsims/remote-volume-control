34a91841e4750b06d2a5cf17b4ec502593e98b5b
Austin Sims <adsims2001@gmail.com>
2016-01-26 18:10:09 -0700

    save the ip address between app opens

--- android/app/src/main/java/name/austinsims/remotevolumecontrol/MainActivity.java
+++ android/app/src/main/java/name/austinsims/remotevolumecontrol/MainActivity.java
@@ -1,10 +1,12 @@
 package name.austinsims.remotevolumecontrol;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,11 +17,14 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
     static final int PORT = 8001;
+    static final String PREFS_NAME = "remote-volume-control";
+    static final String KEY_IP_ADDRESS = "ip";
 
     private TextView ipAddressField;
     private Button volumeUpButton;
     private Button volumeDownButton;
     private Button toggleMuteButton;
+    private SharedPreferences prefs;
 
     private String getHostName() {
         return ipAddressField.getText().toString();
@@ -42,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -51,9 +56,14 @@ public class MainActivity extends AppCompatActivity {
         volumeDownButton = (Button) findViewById(R.id.volumeDownButton);
         toggleMuteButton = (Button) findViewById(R.id.toggleMuteButton);
 
+        prefs = getApplicationContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        String savedIpAddressOrEmptyString = prefs.getString(KEY_IP_ADDRESS, "");
+        ipAddressField.setText(savedIpAddressOrEmptyString);
+
         volumeUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                saveIpAddress();
                 new CommandTask().execute(Character.valueOf('u'));
             }
         });
@@ -61,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
         volumeDownButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                saveIpAddress();
                 new CommandTask().execute(Character.valueOf('d'));
             }
         });
@@ -68,9 +79,17 @@ public class MainActivity extends AppCompatActivity {
         toggleMuteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                saveIpAddress();
                 new CommandTask().execute(Character.valueOf('m'));
             }
         });
+    }
+
+    private void  saveIpAddress() {
+        String ipAddressString = ipAddressField.getText().toString();
+        prefs.edit()
+                .putString(KEY_IP_ADDRESS, ipAddressString)
+                .apply();
     }
 
     private void toast(Exception ex) {
